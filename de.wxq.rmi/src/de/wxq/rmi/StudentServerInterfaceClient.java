7b1a3c6499fcf5feb81f9cd9c7e1c4146ea0b6e2
wang <wang@192.168.129.42>
2012-11-09 15:16:32 +0100

    RMI client implemented.

--- de.wxq.rmi/src/de/wxq/rmi/StudentServerInterfaceClient.java
+++ de.wxq.rmi/src/de/wxq/rmi/StudentServerInterfaceClient.java
@@ -1,5 +1,9 @@
 package de.wxq.rmi;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -7,6 +11,12 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 /**
  * An applet as a client of RMI.<br>
@@ -27,18 +37,55 @@ public class StudentServerInterfaceClient extends JApplet {
     /** applet or application */
     private boolean isStandalone;
 
+    private JButton jbtGetScore = new JButton("Get Score");
+    private JTextField jtfName = new JTextField();
+    private JTextField jtfScore = new JTextField();
+
     @Override
     public void init() {
 	initializeRMI();
 
+	JPanel jPanel1 = new JPanel();
+	jPanel1.setLayout(new GridLayout(2, 2));
+	jPanel1.add(new JLabel("Name"));
+	jPanel1.add(jtfName);
+	jPanel1.add(new JLabel("Score"));
+	jPanel1.add(jtfScore);
+
+	add(jbtGetScore, BorderLayout.SOUTH);
+	add(jPanel1, BorderLayout.CENTER);
+
+	jbtGetScore.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent evt) {
+		getScore();
+	    }
+	});
+    }
+
+    private void getScore() {
+	double score;
+	try {
+	    score = student.findScore(jtfName.getText().trim());
+
+	    if (score < 0) {
+		jtfScore.setText("Not found");
+	    } else {
+		jtfScore.setText(String.valueOf(score));
+	    }
+
+	} catch (RemoteException e) {
+	    e.printStackTrace();
+	}
     }
 
     private void initializeRMI() {
-	String host = " ";
+	String host = "localhost";
 
 	if (!isStandalone) {
 	    final URL codeBase = getCodeBase();
 	    host = codeBase.getHost();
+	    System.out.println("Host: " + host);
 	}
 	
 	try {
@@ -50,6 +97,20 @@ public class StudentServerInterfaceClient extends JApplet {
 	} catch (RemoteException | NotBoundException e) {
 	    e.printStackTrace();
 	}
+    }
+
+    public static void main(String[] args) {
+	StudentServerInterfaceClient applet = new StudentServerInterfaceClient();
+	applet.isStandalone = true;
+
+	JFrame frame = new JFrame();
+	frame.setTitle("StudentServerInterfaceClient");
+	frame.add(applet, BorderLayout.CENTER);
+	frame.setSize(250, 150);
+	applet.init();
+	frame.setLocationRelativeTo(null);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
 }
